c09d090b11581ab0dcc90fb7f698146fe5754dfe
Marcin Pudełek <mrcnpdlk@gmail.com>
2017-09-14 23:46:05 +0200

    refactor + rearrange code

--- src/mrcnpdlk/Regon/Enum/Connection.php
+++ src/mrcnpdlk/Regon/Enum/Connection.php
@@ -9,7 +9,7 @@
  * For the full copyright and license information, please view source file
  * that is bundled with this package in the file LICENSE
  *
- * @author Marcin Pudełek <marcin@pudelek.org.pl>
+ * @author  Marcin Pudełek <marcin@pudelek.org.pl>
  */
 
 /**
